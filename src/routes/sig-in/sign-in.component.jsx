76acb83effb0bb5cb90b1bf2c870ccd0d606904b
Jon Kaplon <jon.kaplon1@gmail.com>
2022-08-31 08:18:34 -0700

    auth with firebase

--- src/routes/sig-in/sign-in.component.jsx
+++ src/routes/sig-in/sign-in.component.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils.js";
 
-function SignIn() {
-  return <div>sign-in.component</div>;
-}
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+    </div>
+  );
+};
 
 export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqt6JwZHJD-TGBW6yVeldU4t-aPNehjJ4",
+  authDomain: "jkap-clothing-db.firebaseapp.com",
+  projectId: "jkap-clothing-db",
+  storageBucket: "jkap-clothing-db.appspot.com",
+  messagingSenderId: "466772596362",
+  appId: "1:466772596362:web:d12a7a4ae484ce6fef3f73",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
