8de99b1c1c419d99cf4c5ed4e883d5c9694b43ef
Jon Kaplon <jon.kaplon1@gmail.com>
2022-08-31 08:03:05 -0700

    firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import Home from "./routes/home/home.component.jsx";
+import SignIn from "./routes/sig-in/sign-in.component.jsx";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/signIn" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -14,6 +14,9 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
+          <Link className="nav-link" to="/signIn">
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
